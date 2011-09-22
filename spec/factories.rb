0265a2427af2d4f5e6372ab037cb99af262a95e5
Steve Tipton <steve.tipton@employmentguide.com>
2011-09-22 15:47:06 -0400

    factory for :user

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
 
+  factory :user do
+    email                   { Forgery(:internet).email_address }
+    password                { Forgery(:basic).password }
+    password_confirmation   { password }
+  end
+
   factory :job_seeker do
     email                   { Forgery(:internet).email_address }
     password                { Forgery(:basic).password }
